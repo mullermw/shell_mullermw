fab01453f9158a9e8686c342f08c76ccbd67fc3e
Matthew Muller <mullermw@gmail.com>
2015-06-22 15:37:09 -0700

    removing need for /Users/mullermw/swprojects/shell_mullermw

--- copy_files_from_home.sh
+++ copy_files_from_home.sh
@@ -2,17 +2,20 @@
 
 # Copy the files tracked by this project from the current user's home directory to the project directory.
 
-if [ ! -d "$SHELL_MULLERMW_HOME" ]; then
-    echo '$SHELL_MULLERMW_HOME must be set to a valid directory'
-    exit 1;
-fi 
+# SET $SOURCE to the PATH TO THIS SCRIPT FILE
+# SET $DIR to the PATH TO THE PARENT DIRECTORY OF $SOURCE
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-#protype home directory
-HOME_PROTO=$SHELL_MULLERMW_HOME/home
+#protype home directory in this project
+HOME_PROTO=$DIR/home
 
 cp ~/.profile $HOME_PROTO
 cp ~/.bashrc $HOME_PROTO
 
 exit 0;
-
-
