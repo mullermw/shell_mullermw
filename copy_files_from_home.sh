de106983080bbcfcc91f99b7e7e2e97138d3b508
Matthew Muller <mullermw@gmail.com>
2015-06-22 15:40:41 -0700

    adding a comment

--- copy_files_from_home.sh
+++ copy_files_from_home.sh
@@ -4,6 +4,7 @@
 
 # SET $SOURCE to the PATH TO THIS SCRIPT FILE
 # SET $DIR to the PATH TO THE PARENT DIRECTORY OF $SOURCE
+# from http://stackoverflow.com/a/246128
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
